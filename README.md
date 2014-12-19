b171bf8e017da9459b21923f45cf8c5f274486dc
Joeri Verdeyen <joeriv@yappa.be>
2014-12-19 15:43:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+==========================
+packer-template
+==========================
+
+Packer template for building a base development box.
+
+Usage
+=====
+
+Installing Packer
+-----------------
+
+Download the latest packer from http://www.packer.io/downloads.html and unzip the appropriate directory.
+
+If you're using Homebrew
+
+::
+
+    $ brew tap homebrew/binary
+    $ brew install packer
+
+
+Running Packer
+--------------
+
+::
+    $ git clone https://github.com/yappabe/packer-template
+    $ cd packer-template
+    $ git clone https://github.com/yappabe/ansible-roles
+    $ packer build template.json
+
+
+Supported versions
+------------------
+
+This templates was tested using a packer 0.7.2 .
---
+++ scripts/cleanup.sh
@@ -0,0 +1,35 @@
+echo "cleaning up apt stuff"
+apt-get -y autoremove -y
+apt-get clean -y
+apt-get autoclean -y
+
+echo "cleaning up guest additions"
+rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
+
+echo "cleaning up dhcp leases"
+rm /var/lib/dhcp/*
+
+echo "cleaning up udev rules"
+rm /etc/udev/rules.d/70-persistent-net.rules
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /dev/.udev/
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
+
+echo "remove APT files"
+find /var/lib/apt -type f | xargs rm -f
+
+echo "remove documentation files"
+find /var/lib/doc -type f | xargs rm -f
+
+echo "remove Virtualbox specific files"
+rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest*
+
+echo "remove Linux headers"
+rm -rf /usr/src/linux-headers*
+
+echo "remove logs"
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+
+echo "remove cache"
+find /var/cache -type f -exec rm -rf {} \;
+
